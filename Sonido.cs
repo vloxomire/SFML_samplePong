6c4a240d5e8383e9ca000a24e258c36fede35776
Max Sebastián Saldaña <vloxomire@gmail.com>
2020-08-13 22:14:34 -0300

    audio try

--- Sonido.cs
+++ Sonido.cs
@@ -4,17 +4,19 @@ using System;
 
 namespace SFML_sample
 {
+    public static String[] ubicacion = new String[] { "sfx/canario.wav", "sfx/clash.wav" };
     static class Sonido
     {
-        public static void PlaySonido() 
+        public static void PlaySonido(String ubicacion)
         {
-            var buffer = new SoundBuffer("musica/canario.wav");
+            var buffer = new SoundBuffer(ubicacion);
             var sonido = new Sound(buffer);
+
             sonido.Play();
         }
         public static void PlayMusica() 
         {
-            var musica = new Music("music/orquesta.ogg");
+            var musica = new Music("sfx/orquesta.ogg");
             musica.Play();
         }
     }
